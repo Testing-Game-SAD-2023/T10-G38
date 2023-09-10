dd06fd9fc2329d23532d1fb4beb4aef2d7c22c2c
Domenico Chianese <61273932+Star-Lord76@users.noreply.github.com>
2023-09-10 18:46:26 +0200

    Update

--- install.bat
+++ install.bat
@@ -1,25 +1,36 @@
-ECHO Installazione iniziale e avvio dei container
-ECHO Creazione volume condiviso
-docker volume create shared_data
-ECHO Creazione container ClassRepo
-cd T1-G20-main\T1-G20-main\classUT-repository 
-docker build -t t1 . --no-cache
-docker-compose up -d 
-ECHO Creazione contanier front-end
-cd ..\..\..\T5-G5-main\T5-G5-main
-xcopy build \T5-G5-main\ /s /Y 
-cd T5-G5-main
-docker build -t t5front . --no-cache
-docker-compose up -d
-ECHO Creazione container back-end
-cd backend\
-docker build -t t5back . --no-cache
-docker-compose up -d 
-ECHO Creazione container editor
-cd ..\..\..\..\T6-G16-main\T6-G16-main\spring\editor 
-docker build -t t6 . --no-cache
-docker run -p 8190:8080 -d -v shared_data:/FileSystem --name editor t6 
-ECHO Creazione container compiler
-cd ..\..\..\..\T7-G23-main\T7-G23-main 
-docker build -t t7 . --no-cache
-docker run -p 5000:8080 -d -v shared_data:/FileSystem --name compiler t7
+	::creazione del volume condiviso
+	docker volume create shared_data
+	
+	//reset dei file necessari al container di mantenimento delle classi
+	cd T1-G20-main\T1-G20-main\classUT-repository
+	mkdir tmp
+	robocopy /mir tmp .\ClassiUT\
+	robocopy /mir tmp .\database\ 
+	rmdir tmp
+	//build e compose up dell'immagine
+	docker build -t t1 . --no-cache
+	docker-compose up -d
+	
+	//reset dei file necessari al game engine
+	cd ..\..\..\T5-G5-main\T5-G5-main
+	xcopy build \T5-G5-main\ /s /Y 
+	cd T5-G5-main
+	//build e compose up dell'immagine del front-end
+	docker build -t t5front . --no-cache
+	docker-compose up -d
+	cd backend\
+	//build e compose up dell'immagine del back-end
+	docker build -t t5back . --no-cache
+	docker-compose up -d
+	
+	//creazione di immagine e container editor 
+	cd ..\..\..\..\T6-G16-main\T6-G16-main\spring\editor 
+	docker build -t t6 . --no-cache
+	//esecuzione del container su porta :8190 e montaggio del volume condiviso
+	docker run -p 8190:8080 -d -v shared_data:/FileSystem --name editor t6
+	
+	//creazione di immagine e container compilatore
+	cd ..\..\..\..\T7-G23-main\T7-G23-main 
+	docker build -t t7 . --no-cache
+	//esecuzione del container su porta :5000 e montaggio del volume condiviso
+	docker run -p 5000:8080 -d -v shared_data:/FileSystem --name compiler t7
